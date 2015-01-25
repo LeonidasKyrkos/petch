a57ee09336949ae90ecea5eb6ddb4c3ed86a057b
Leonidas Kyrkos <leo.kyrkos@outlook.com>
2015-01-25 23:13:41 +0000

    a few small changes to look

--- app/js/src/app.js
+++ app/js/src/app.js
@@ -4,3 +4,16 @@ var $ = require('jquery');
 
 console.log("jQuery verison: "+$().jquery);
 
+
+
+(function(){
+
+	var $playButton = $('.js-play');
+
+	$playButton.on('click',activate);
+
+	function activate() {
+		$playButton.toggleClass('hide');
+	}
+
+})();
